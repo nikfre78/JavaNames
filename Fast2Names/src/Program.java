3f389c3f84e868af8d04220852ac71be17cb4f8c
Niklas <Niklas@HPm9441sc.lan>
2016-11-06 18:16:42 +0100

    Commit and push test

--- Fast2Names/src/Program.java
+++ Fast2Names/src/Program.java
@@ -4,6 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		System.out.println();
+		System.out.println("Welcome");
+		System.out.println();
+
 		String filter = "";
 		String evenOdd = "";
 		boolean sortAscending = true;
